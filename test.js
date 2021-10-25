2571b91e7eb545022ea43d2ad23df88cda37ea4f
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-25 17:21:32 -0500

    commit

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('say hello again', () => {
+  expect(getGreeting('Jared')).toBe("Hi Jared")
+});
